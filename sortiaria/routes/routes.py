ff81dcbc385b8b39ed65c2d690f6893cbcf61f31
lafrering <lafrering@github.com>
2018-03-19 15:53:43 +0100

    débug routes.py / issue#43

--- sortiaria/routes/routes.py
+++ sortiaria/routes/routes.py
@@ -13,7 +13,7 @@ def accueil():
     """ Route permettant l'affichage d'une page accueil
     """
     # On a bien sûr aussi modifié le template pour refléter le changement
-    lieux = Place.query.order_by(Place.place_id.desc()).limit(5).all()
+    mots = Mot.query.order_by(Mot.mot_id.desc()).limit(5).all()
     return render_template("pages/accueil.html", nom="Sortiaria", mots=mots)
 
 
