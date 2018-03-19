9c61f5dc1ccfa11146628f1e3495e754ec4b0334
AlyxTaj <alyx.taounza-jeminet@enc-sorbonne.fr>
2018-03-19 16:01:44 +0100

    modification run.py

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 from sortiaria.app import app
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app = config_app("production")
-=======
->>>>>>> 6f35b4d0d51ea710eda8014e2c73b9f567cc61fa
     app.run(debug=True)
