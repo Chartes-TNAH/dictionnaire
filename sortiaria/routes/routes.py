5c07d175be637ec407f8b3e9b3803dd6546d0943
lafrering <lafrering@github.com>
2018-04-03 17:44:19 +0200

    patch recherche.html et changement de la route du template dans routes.py /issue#70

--- sortiaria/routes/routes.py
+++ sortiaria/routes/routes.py
@@ -62,7 +62,7 @@ def recherche():
         titre = "Résultat pour la recherche `" + motclef + "`"
 
     return render_template(
-        "partials/recherche.html",
+        "pages/recherche.html",
         resultats=resultats,
         titre=titre,
         keyword=motclef
